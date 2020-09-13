b5d9812795a02ee5c1131f9536c3a76d08d38702
Sam Bauch <sbauch@gmail.com>
2020-09-13 10:01:42 -0400

    latest package

--- app.rb
+++ app.rb
@@ -23,15 +23,10 @@ class App < Sinatra::Base
   set :expose_headers, 'location,link'
 
   get '/' do
-    redirect '/admin/enterprise' # if ENV['RACK_ENV'] == 'development'
+    redirect '/admin/enterprise'
   end
 
   get '/health' do
     'ok'
-  end
-
-  # TODO: move to gem and build the actual PDF writer
-  get '/identity_provider/documentation/:id' do
-    send_file('okta.pdf', disposition: 'attachment', filename: 'Okta Setup Docs.pdf')
   end
 end
