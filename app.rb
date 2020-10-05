e8e80141cdb2418a25561aabac24980383288fe2
Sam Bauch <sbauch@gmail.com>
2020-10-05 10:18:28 -0400

    cors updates from upstream

--- app.rb
+++ app.rb
@@ -1,28 +1,14 @@
 # frozen_string_literal: true
 
-require 'rack/ssl-enforcer'
 require 'sinatra/reloader'
-require 'sinatra/cors'
 require 'osso'
-require_relative 'lib/osso/helpers/auth'
 
 class App < Sinatra::Base
-  use Rack::SslEnforcer
   include Osso::AppConfig
   include Osso::Helpers::Auth
   include Osso::RouteMap
-
-  configure :development do
-    register Sinatra::Reloader
-  end
-
+  
   register Sinatra::ActiveRecordExtension
-  register Sinatra::Cors
-
-  set :allow_origin, '*'
-  set :allow_methods, 'GET,HEAD,POST,OPTIONS'
-  set :allow_headers, 'content-type,if-modified-since'
-  set :expose_headers, 'location,link'
 
   get '/' do
     redirect '/admin/enterprise'
