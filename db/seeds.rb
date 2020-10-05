8fbbd9406e16b7b40553056adb978fa454ef1a3c
Sam Bauch <sbauch@gmail.com>
2020-10-05 15:34:29 -0400

    better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@ Osso::Models::EnterpriseAccount.create!(
   domain: 'customer.com',
   oauth_client: oauth_client,
 )
+
+Osso::Models::AppConfig.create!(
+  name: 'SaaS App',
+  contact_email: 'help@saasapp.com',
+  logo_url: 'https://avatars1.githubusercontent.com/u/62798259?s=400&u=7df069faddffb0ef6b52d015a7dba4e61a01dab7&v=4'
+)
