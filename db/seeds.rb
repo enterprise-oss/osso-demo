1f26967406edf906e312f284cd4e032ab8eeb97a
Sam Bauch <sbauch@gmail.com>
2021-02-01 14:58:49 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,74 +23,15 @@ hlYTLC++fXIqgXlZXFwjjoWmze9+xWs+JXCN8K9hZ+YA13E8kLKcTHAD
 -----END CERTIFICATE-----
 CERT
 
-callback_uris = [
-  'https://sp.ossoapp.io/auth/osso/callback',
-  'http://localhost:4567/auth/osso/callback', # sinatra omniauth
-  'http://127.0.0.1:3000/users/auth/osso/callback', # devise (rails) omniauth
-  'http://localhost:3000/users/auth/osso/callback', # devise (rails) omniauth
-  'http://localhost:3000/auth/osso/callback', # rails omniauth
-  'http://localhost:8000/auth/osso/callback', # node passport-osso
-  'https://nextjs-demo.ossoapp.com/api/auth/callback/osso',
-  'http://localhost:9292/health', # CI
-]
-
-oauth_client = Osso::Models::OauthClient.create!(
-  name: 'Demo Production',
-  identifier: 'demo-client-id',
-  secret: 'demo-client-secret'
-)
-
-callback_uris.each_with_index do |uri, index|
-  Osso::Models::RedirectUri.create(
-    oauth_client: oauth_client,
-    uri: uri,
-    primary: index == 0,
-  )
-end
-
-customer = Osso::Models::EnterpriseAccount.create!(
-  name: 'SaaS Co Customer',
-  domain: 'example.com',
-)
-
-test_idp = Osso::Models::IdentityProvider.create!(
-  sso_cert: demo_idp_cert,
-  sso_url: 'https://idp.ossoapp.com/saml-login',
-  domain: 'example.com',
-  enterprise_account: customer,
-  oauth_client: oauth_client,
-  status: 'ACTIVE',
-  service: 'AZURE' # TODO: add either generic SAML or Osso Test as providers
-)
-
-demo_idp_cert = ENV['ENTERPRISE_OSS_OKTA_CERT']
-
-if demo_idp_cert
-  demo_customer = Osso::Models::EnterpriseAccount.create!(
-    name: 'EnterpriseOSS',
-    domain: 'enterpriseoss.dev',
-  )
-
-  demo_idp = Osso::Models::IdentityProvider.create!(
-    id: "5d606919-1717-41ba-bfa3-8abda6cfea8e",
-    sso_cert: demo_idp_cert,
-    sso_url: 'https://dev-634049.okta.com/app/dev-634049_ossodemo_1/exk1m96glaUlmZ2A04x7/sso/saml',
-    domain: 'enterpriseoss.dev',
-    enterprise_account: demo_customer,
-    oauth_client: oauth_client,
-    status: 'ACTIVE',
-    service: 'OKTA'
-  )
-
-  # TODO: our before_create hook means we can't provide this as a create option
-  demo_idp.update(sso_issuer: 'enterpriseoss.dev')
-end
+# App Config
 
 Osso::Models::AppConfig.create!(
   name: 'SaaS App',
-  contact_email: 'help@saasapp.com',
-  logo_url: 'https://avatars1.githubusercontent.com/u/62798259?s=400&u=7df069faddffb0ef6b52d015a7dba4e61a01dab7&v=4'
+  contact_email: 'hello@ossoapp.com',
+  logo_url: 'https://assets.ossoapp.io/icons/OGImage.png'
 )
+
+# Admin Account
 
 admin = Osso::Models::Account.create!(
   email: 'admin@saas.co',
@@ -103,4 +44,112 @@ ActiveRecord::Base.connection.execute(
     INSERT INTO account_password_hashes(id, password_hash) 
     VALUES (#{"'" + admin.id + "'"}, #{"'" + BCrypt::Password.create("password", cost: BCrypt::Engine::MIN_COST).to_s + "'"});
   SQL
+)
+
+# example.com
+
+example_com_customer = Osso::Models::EnterpriseAccount.create!(
+  name: 'SaaS Co Customer',
+  domain: 'example.com',
+)
+
+# Demo
+
+demo_callback_uris = [
+  'http://localhost:4567/auth/osso/callback', # sinatra omniauth
+  'http://localhost:3000/auth/osso/callback', # rails omniauth
+  'http://localhost:8000/auth/osso/callback', # node passport-osso
+  'http://localhost:9292/health', # CI
+]
+
+demo_oauth_client = Osso::Models::OauthClient.create!(
+  name: 'Demo Production',
+  identifier: 'demo-client-id',
+  secret: 'demo-client-secret'
+)
+
+demo_callback_uris.each_with_index do |uri, index|
+  Osso::Models::RedirectUri.create(
+    oauth_client: oauth_client,
+    uri: uri,
+    primary: index == 0,
+  )
+end
+
+# Mock IDP
+
+Osso::Models::IdentityProvider.create!(
+  sso_cert: demo_idp_cert,
+  sso_url: 'https://idp.ossoapp.com/saml-login',
+  domain: 'example.com',
+  enterprise_account: example_com_customer,
+  oauth_client: demo_oauth_client,
+  status: 'ACTIVE',
+  service: 'AZURE' # TODO: add either generic SAML or Osso Test as providers
+)
+
+# Rails Demo
+
+rails_oauth_client = Osso::Models::OauthClient.create!(
+  name: 'Rails Demo',
+  identifier: 'rails-demo-client-id',
+  secret: 'rails-demo-client-secret'
+)
+
+rails_callback_uris = [
+  'https://rails-demo.ossoapp.com/auth/osso/callback',
+  'http://127.0.0.1:3000/users/auth/osso/callback', # devise (rails) omniauth
+  'http://localhost:3000/users/auth/osso/callback', # devise (rails) omniauth
+]
+
+rails_callback_uris.each_with_index do |uri, index|
+  Osso::Models::RedirectUri.create(
+    oauth_client: rails_oauth_client,
+    uri: uri,
+    primary: index == 0,
+  )
+end
+
+okta_idp_cert = ENV['ENTERPRISE_OSS_OKTA_CERT']
+
+if okta_idp_cert
+  enterprise_oss_customer = Osso::Models::EnterpriseAccount.create!(
+    name: 'EnterpriseOSS',
+    domain: 'enterpriseoss.dev',
+  )
+
+  okta_idp = Osso::Models::IdentityProvider.create!(
+    id: "5d606919-1717-41ba-bfa3-8abda6cfea8e",
+    sso_cert: okta_idp_cert,
+    sso_url: 'https://dev-634049.okta.com/app/dev-634049_ossodemo_1/exk1m96glaUlmZ2A04x7/sso/saml',
+    domain: 'enterpriseoss.dev',
+    enterprise_account: enterprise_oss_customer,
+    oauth_client: rails_oauth_client,
+    status: 'ACTIVE',
+    service: 'OKTA'
+  )
+
+  # TODO: our before_create hook means we can't provide this as a create option
+  demo_idp.update(sso_issuer: 'enterpriseoss.dev')
+end
+
+
+next_js_callback_uris = [
+  'https://nextjs-demo.ossoapp.com/api/auth/callback/osso',
+]
+
+nextjs_oauth_client = Osso::Models::OauthClient.create!(
+  name: 'NextJS Demo',
+  identifier: 'nextjs-demo-client-id',
+  secret: 'nextjs-demo-client-secret'
+)
+
+Osso::Models::IdentityProvider.create!(
+  sso_cert: example_com_customer,
+  sso_url: 'https://idp.ossoapp.com/saml-login',
+  domain: 'example.com',
+  enterprise_account: customer,
+  oauth_client: nextjs_oauth_client,
+  status: 'ACTIVE',
+  service: 'AZURE' # TODO: add either generic SAML or Osso Test as providers
 )
